62741f6203ba670071580df6435c94079729ba20
hankMBP <hunkyguy@163.com>
2016-03-29 10:10:35 +0800

    add msg service to routes

--- src/main/scala/org/hank/kafray/Routes.scala
+++ src/main/scala/org/hank/kafray/Routes.scala
@@ -3,8 +3,10 @@ package org.hank.kafray
 /**
   * Created by ihank on 16/3/25.
   */
-trait Routes extends EchoService with ClientConfigService{
+trait Routes extends MsgService with EchoService with ClientConfigService{
     val routes =
         echoRoute ~
-            clientConfigRoute
+            clientConfigRoute ~
+            msgRoute
+
 }
